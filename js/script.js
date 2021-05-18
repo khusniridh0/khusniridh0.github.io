0e7ef146dae77072d30fb3496cdd024912b3e8de
Khusniridh0 <pribad9983@gmail.com>
2021-05-18 16:44:38 +0900

    New Update

--- js/script.js
+++ js/script.js
@@ -1,59 +1,80 @@
 const menu = document.querySelector('.menu');
 const navItem = document.querySelectorAll('.nav-item');
+const btn =  document.querySelector('.smooth-btn');
 
 
- menu.addEventListener('click', function(e){
- 	// navItem.classList.remove('active');
+menu.addEventListener('click', function(e){
+ 	let element = e.target;
+ 	e.preventDefault();
+ 	let to = document.querySelector(element.hash).offsetTop;
+ 	let scrollto = document.documentElement.scrollTop = (to - 140);
+});
 
- 	for (let i = navItem.length - 1; i >= 0; i--) {
- 		navItem[i].classList.remove('active');
- 	}
- 	e.target.classList.add('active');
- 	
- });
+
+btn.addEventListener('click', function(e){
+	let btn = e.target;
+ 	e.preventDefault();
+ 	let to = document.querySelector(btn.hash).offsetTop;
+ 	let scrollto = document.documentElement.scrollTop = (to - 59);
+});
+
+
+
 
 window.addEventListener('load', function (e) {
-	let scroll = document.documentElement.scrollTop;
 	const load = document.querySelector('.loding');
- 	if (scroll <= 671) {
- 		for (let i = navItem.length - 1; i >= 0; i--) {
- 		navItem[i].classList.remove('active');
- 		}
- 		navItem[0].classList.add('active');
- 	}else if (scroll <= document.getElementById('skill').offsetTop) {
- 		for (let i = navItem.length - 1; i >= 0; i--) {
- 		navItem[i].classList.remove('active');
- 		}
- 		navItem[1].classList.add('active');
- 	}else if (scroll <= document.getElementById('education').offsetTop) {
- 		for (let i = navItem.length - 1; i >= 0; i--) {
- 		navItem[i].classList.remove('active');
- 		}
- 		navItem[2].classList.add('active');
- 	};
-
  	load.style.transition = '0.2';
  	load.style.display = 'none';
+
+ 	let home = document.querySelector('#home').offsetTop + document.querySelector('#home').offsetHeight;
+		home += document.querySelector('#profil').offsetHeight;
+		home -= 140;
+	let skill = document.querySelector('#skill').offsetTop + document.querySelector('#skill').offsetHeight;
+		skill -= 140;
+	let education = document.querySelector('#education').offsetTop + document.querySelector('#education').offsetHeight;
+
+	if (document.documentElement.scrollTop <= home) {
+		for (let i = navItem.length - 1; i >= 0; i--) {
+			navItem[i].classList.remove('active');
+		}
+		navItem[0].classList.add('active');
+	} else if (document.documentElement.scrollTop <= skill) {
+		for (let i = navItem.length - 1; i >= 0; i--) {
+			navItem[i].classList.remove('active');
+		}
+		navItem[1].classList.add('active');
+	}else if (document.documentElement.scrollTop <= education) {
+		for (let i = navItem.length - 1; i >= 0; i--) {
+			navItem[i].classList.remove('active');
+		}
+		navItem[2].classList.add('active');
+	}
 });
 
 
 window.addEventListener('scroll', function(e){
- 	let scroll = document.documentElement.scrollTop;
- 	if (scroll <= 671) {
- 		for (let i = navItem.length - 1; i >= 0; i--) {
- 		navItem[i].classList.remove('active');
- 		}
- 		navItem[0].classList.add('active');
- 	}else if (scroll <= document.getElementById('skill').offsetTop) {
- 		for (let i = navItem.length - 1; i >= 0; i--) {
- 		navItem[i].classList.remove('active');
- 		}
- 		navItem[1].classList.add('active');
- 	}else if (scroll <= document.getElementById('education').offsetTop) {
- 		for (let i = navItem.length - 1; i >= 0; i--) {
- 		navItem[i].classList.remove('active');
- 		}
- 		navItem[2].classList.add('active');
- 	};
+	let home = document.querySelector('#home').offsetTop + document.querySelector('#home').offsetHeight;
+		home += document.querySelector('#profil').offsetHeight;
+		home -= 140;
+	let skill = document.querySelector('#skill').offsetTop + document.querySelector('#skill').offsetHeight;
+		skill -= 140;
+	let education = document.querySelector('#education').offsetTop + document.querySelector('#education').offsetHeight;
 
+	if (document.documentElement.scrollTop <= home) {
+		for (let i = navItem.length - 1; i >= 0; i--) {
+			navItem[i].classList.remove('active');
+		}
+		navItem[0].classList.add('active');
+	} else if (document.documentElement.scrollTop <= skill) {
+		for (let i = navItem.length - 1; i >= 0; i--) {
+			navItem[i].classList.remove('active');
+		}
+		navItem[1].classList.add('active');
+	}else if (document.documentElement.scrollTop <= education) {
+		for (let i = navItem.length - 1; i >= 0; i--) {
+			navItem[i].classList.remove('active');
+		}
+		navItem[2].classList.add('active');
+	}
+	
 });
