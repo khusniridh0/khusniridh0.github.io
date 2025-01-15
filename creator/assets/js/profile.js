fa9c6493f240de4d4ce473c1aa41a91bcd2aa83d
khusniridh0 <pribad9983@gmail.com>
2025-01-15 09:44:09 +0700

    update

--- creator/assets/js/profile.js
+++ creator/assets/js/profile.js
@@ -4,35 +4,35 @@
 	Version      : 0.2
 */
 
-(function($) {
+(function ($) {
 	"use strict";
 
 	// Declaret navbar
 	let navHeight = $('nav').outerHeight();
 
 	//**================= Requerst data =====================**//
-	$.ajax({
-		url: 'https://projek-ts81.000webhostapp.com/',
-		method: 'get',
-		dataType: 'json',
-		success: function(res) {
-			$('.caption-fesrt p').html(res.about.first)
-			$('.caption-last p').html(res.about.last)
+	// $.ajax({
+	// 	url: 'https://projek-ts81.000webhostapp.com/',
+	// 	method: 'get',
+	// 	dataType: 'json',
+	// 	success: function(res) {
+	// 		$('.caption-fesrt p').html(res.about.first)
+	// 		$('.caption-last p').html(res.about.last)
 
-			let newItems = $('.carousel-item').clone()
-			$.each(res.certificate, function (i, v) {
-				newItems.find('img').attr('src', v)
-			})
-			$('.carousel-inner').append(newItems)
-		},
-	});
+	// 		let newItems = $('.carousel-item').clone()
+	// 		$.each(res.certificate, function (i, v) {
+	// 			newItems.find('img').attr('src', v)
+	// 		})
+	// 		$('.carousel-inner').append(newItems)
+	// 	},
+	// });
 
 	//**================= Preloader =====================**//
-	$(window).on('load', function() { 
+	$(window).on('load', function () {
 		$('.atf-status').fadeOut()
 		$('.atf-preloader').delay(350).fadeOut('slow')
 		$('.atf-certificat').fadeOut()
-	}); 
+	});
 
 	//**================= Certificat =====================**//
 	$('.certificat').click(function (e) {
@@ -48,19 +48,19 @@
 		$('.atf-certificat').removeClass('active')
 	})
 
-	$(document).keyup(function(e) {
-		if (e.keyCode === 27 && $('.atf-certificat').hasClass('active')) { 
+	$(document).keyup(function (e) {
+		if (e.keyCode === 27 && $('.atf-certificat').hasClass('active')) {
 			$('.atf-certificat').delay(350).fadeOut()
 			$('.atf-certificat').removeClass('active')
 		}
 	})
 
 	//**================= Form =====================**//
-	$(document).on('ready', function() { 
+	$(document).on('ready', function () {
 		$('.form-input').each(function (i, e) {
 			if ($(e).val().length > 0) $(e).addClass('active')
 		})
-	}); 
+	});
 
 	$('.form-input').keyup(function () {
 		if ($(this).val().length > 0) {
@@ -118,7 +118,7 @@
 				$('.form-message').html('Pesan telalu panjang')
 				$('.form-message').addClass('show')
 				$('.form-message').addClass('error')
-				hide_msg()	
+				hide_msg()
 			}
 		} else {
 			$('.form-message').html('Pesan tidak boleh kosong')
@@ -132,7 +132,7 @@
 				url: 'https://projek-ts81.000webhostapp.com/',
 				method: 'post',
 				dataType: 'json',
-				data: {name, email, message},
+				data: { name, email, message },
 				success: function (data) {
 					console.log(data);
 					$('.form-message').html('Pesan berhasil dikirim')
@@ -154,8 +154,8 @@
 	}
 	//**================= Section background style =====================**//
 	$('section').each(function (i) {
-		let e = i%2;
-		if (e == 0) {			
+		let e = i % 2;
+		if (e == 0) {
 			$('section').eq(i).css('background-color', '#2b1d6a')
 		}
 	})
@@ -177,7 +177,7 @@
 	//**================= Active scrollspy =====================**//
 	$('body').scrollspy({
 		target: '#menu-top',
-		offset: navHeight*2+40
+		offset: navHeight * 2 + 40
 	})
 
 	//**================= Navbar reduce, Custom scroll =====================**//
@@ -195,17 +195,17 @@
 
 	// Custom scroll
 	function customScroll() {
-		let height = $(document).scrollTop()/($(document).height() - $(window).height())*100
+		let height = $(document).scrollTop() / ($(document).height() - $(window).height()) * 100
 		$('.scroll-bar').css('height', height + 'vh')
 	}
 
 	// Navbar reduce
-	function menuReduce(){
+	function menuReduce() {
 		// reduce
 		$('.navbar-collapse').collapse('hide')
 		if ($(window).scrollTop() > navHeight) {
 			$('#menu-top').addClass('bg-nav')
-		}else{
+		} else {
 			$('#menu-top').removeClass('bg-nav')
 		}
 	}
@@ -232,9 +232,9 @@
 	$(window).ready(function () {
 		let docHeight = $(document).outerHeight()
 		$('.atf-bubling-item').each(function (i) {
-			let width = Math.floor(Math.random()*50)+50
-			let delay = Math.floor(Math.random()*20)
-			let duration = Math.floor(docHeight/(width-delay)-35)
+			let width = Math.floor(Math.random() * 50) + 50
+			let delay = Math.floor(Math.random() * 20)
+			let duration = Math.floor(docHeight / (width - delay) - 35)
 			$('.atf-bubling-item').eq(i).css('width', width + 'px')
 			$('.atf-bubling-item').eq(i).css('animation-delay', delay + 's')
 			$('.atf-bubling-item').eq(i).css('animation-duration', duration + 's')
@@ -254,7 +254,7 @@
 			$(this).children().eq(0).css('transform', 'translateY(-87px)')
 			$(this).children().eq(1).css('transform', 'translateY(-67px)')
 		})
-		$(window).scroll(function() {
+		$(window).scroll(function () {
 			progres()
 		})
 
@@ -268,7 +268,7 @@
 
 	//**================= Typed-word =====================**//
 	let typed = new Typed('.typed-word', {
-		strings: ["Icon Designer","Web Developer"," Web Designer"],
+		strings: ["Icon Designer", "Web Developer", " Web Designer"],
 		typeSpeed: 40,
 		backSpeed: 40,
 		backDelay: 2000,
@@ -279,7 +279,7 @@
 
 	//**================= Typed-word =====================**//
 	let dot = new Typed('.dot', {
-		strings: [". . .",". . ."],
+		strings: [". . .", ". . ."],
 		typeSpeed: 85,
 		backSpeed: 85,
 		backDelay: 0,
@@ -296,7 +296,7 @@
 	wow.init()
 
 	// tilt-js
-	VanillaTilt.init(document.querySelectorAll(".tilt-js"),{
+	VanillaTilt.init(document.querySelectorAll(".tilt-js"), {
 		max: 15,
 		speed: 1000,
 		perspective: 1000,
@@ -305,11 +305,11 @@
 
 	//**================= Owl Carousel =====================**//
 	$('.owl-carousel').owlCarousel({
-		loop:true,
-		autoWidth:false,
-		autoplay:true,
-		autoplayTimeout:5000,
-		autoplayHoverPause:true,
+		loop: true,
+		autoWidth: false,
+		autoplay: true,
+		autoplayTimeout: 5000,
+		autoplayHoverPause: true,
 	})
 
 })(jQuery)
